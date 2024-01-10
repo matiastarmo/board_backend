538e489b8c74dd63a9b80b0f04820f9817e2ae50
matias <matias.tarmo@aalto.fi>
2024-01-10 17:44:37 +0200

    muutokst

--- backend/delpoyment.py
+++ backend/delpoyment.py
@@ -6,5 +6,19 @@ ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = ['*']
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'OPTIONS': {
+            'options': '-c search_path=development'
+        },
+        'NAME': os.environ.get('DB_NAME'), 
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'), 
+        'PORT': os.environ.get('DB_PORT'),
+    }
+}
+
 
 DEBUG = False
