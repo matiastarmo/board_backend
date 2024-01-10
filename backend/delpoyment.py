3d9a5692d3cbfb1480d28b479a7eb3ddbd0f6ef6
matias <matias.tarmo@aalto.fi>
2024-01-10 18:40:44 +0200

    muutoksia

--- backend/delpoyment.py
+++ backend/delpoyment.py
@@ -2,9 +2,9 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 CORS_ALLOWED_ORIGINS = ['*']
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
